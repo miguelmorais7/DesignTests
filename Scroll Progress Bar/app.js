f2c39703da345208633d3110132612f7f2dd57b3
miguelmorais7 <42868106+miguelmorais7@users.noreply.github.com>
2021-01-04 11:17:07 -0300

    Scroll-Progress-Bar finished

---
+++ Scroll Progress Bar/app.js
@@ -0,0 +1,6 @@
+let progress = document.getElementById('progressbar');
+let totalheight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function(){
+    let progressHeight = (window.pageYOffset / totalheight) * 100;
+    progress.style.height = progressHeight + "%";
+}
